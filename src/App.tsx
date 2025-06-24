2a124f1c3f11f0990ca89d9a4d216901e6ed093b
jacksydenham <jack.sydenham@live.com.au>
2025-06-24 16:10:58 +1000

    coins created

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,73 @@
-import { Canvas } from "@react-three/fiber";
-import "./App.css";
+import * as THREE from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  ScrollControls,
+  Scroll,
+  useScroll,
+  Environment,
+} from "@react-three/drei";
 import Coin from "./components/Coin";
-import { Environment, OrbitControls } from "@react-three/drei";
+import "./App.css";
+import React from "react";
+import "@fontsource/bebas-neue/400.css";
 
-function App() {
+
+function ScrollScene() {
+  const coinState = React.useRef<THREE.Group>(null);
+  const scroll = useScroll();
+
+  useFrame(() => {
+    if (!coinState.current) return;
+    const scrollPos = scroll.offset;
+
+    let x = 0.5, y = 0;
+    
+    if (scrollPos < 0.25) x = THREE.MathUtils.lerp(0.5, 2.5, scrollPos / 0.25); // first 25%
+    
+    else if (scrollPos < 0.75) x = 2.5; // middle 50%
+    
+    else { // last 25%
+      const k = (scrollPos - 0.75) / 0.25; 
+
+      x = 2.5;
+      y = THREE.MathUtils.lerp(0, -1, k);
+    }
+
+    coinState.current.rotation.y += 0.01;
+    coinState.current.position.set(x, y, 0);
+  });
+
   return (
-    <Canvas shadows camera={{ position: [0, 2, 5], fov: 50 }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[2, 4, 2]} intensity={1.2} castShadow />
-
-      <mesh
-        receiveShadow
-        rotation={[-Math.PI / 2, 0, 0]}
-        position={[0, -0.05, 0]}
-      >
-        <planeGeometry args={[10, 10]} />
-        <shadowMaterial opacity={0.2} />
-      </mesh>
-
-      <Coin position={[0, 0, 0]} />
-      <OrbitControls />
+    <group ref={coinState}>
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[2, 4, 3]} intensity={1} />
+      <Coin scale={3} />
       <Environment preset="studio" />
-    </Canvas>
+    </group>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Canvas className="bgCanvas" camera={{ position: [0, 2, 6], fov: 50 }}>
+      <ScrollControls pages={3}>
+        <ScrollScene />
+
+        <Scroll html>
+          <section className="hero"> 
+            <h1>Jack&nbsp;Sydenham</h1>
+            <p>Full-stack&nbsp;Developer</p>
+          </section>
+
+          <section>
+            <h2>Skills + Projects</h2>
+          </section>
+
+          <section>
+            <h2>Skills + Projects</h2>
+          </section>
+        </Scroll>
+      </ScrollControls>
+    </Canvas>
+  );
+}
--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,15 +1,29 @@
+import * as THREE from "three";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 interface CoinProps {
   position?: [number, number, number];
+  scale?: number;
 }
 
-export default function Coin({ position = [0, 0, 0] }: CoinProps) {
+export default function Coin({ position = [0, 0, 0], scale = 5 }: CoinProps) {
   const { nodes } = useGLTF("/models/Coin.glb") as any;
+
+  const ref = useRef<THREE.Mesh>(null);
+
+  useFrame((_, delta) => {
+    if (!ref.current) return;
+    ref.current.rotation.z += delta;
+  });
+
   return (
     <mesh
+      ref={ref}
       geometry={nodes.Coin.geometry}
       position={position}
+      scale={scale}
       castShadow
       receiveShadow
     >
